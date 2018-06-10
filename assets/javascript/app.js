a43f3c622a40821b11839e1492da1e1ce54ac214
StewartBC <stewcapps@gmail.com>
2018-06-10 14:33:51 -0400

    completed all js logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,113 @@
 var trivia = [
     {
         question: "What is the name of Orianna's ultimate ability?",
-        answer1: "Command: Dissonance",
-        answer2: "Command: Protect",
-        answer3: "Command: Shockwave",
-        answer4: "Command: Attack"
+        answers: [
+            "Command: Dissonance",
+            "Command: Protect",
+            "Command: Shockwave",
+            "Command: Attack"
+        ],
+        correctAnswer: "Command: Shockwave",
+        image: "<img src=\"assets/images/Orianna.gif\" alt=\"Orianna Shockwave\">"
     },
     {
         question: "Which champion has an ability called Tidal Wave?",
-        answer1: "Varus",
-        answer2: "Corki",
-        answer3: "Nami",
-        answer4: "Lux"
+        answers: [
+            "Varus",
+            "Corki",
+            "Nami",
+            "Lux"
+        ],
+        correctAnswer: "Nami",
+        image: "<img src=\"assets/images/Nami.gif\" alt=\"Nami Tidal Wave\">"
     },
     {
         question: "Which of the following champions has most recently been introduced to the game?",
-        answer1: "Kled",
-        answer2: "Kayn",
-        answer3: "Zoe",
-        answer4: "Kai'Sa"
+        answers: [
+            "Kled",
+            "Camille",
+            "Taliyah",
+            "Ivern"
+        ],
+        correctAnswer: "Camille",
+        image: "<img src=\"assets/images/Camille.jpg\" alt=\"Camille\">"
     },
     {
         question: "Which of the following items grants the most ability power?",
-        answer1: "Rabadon's Deathcap",
-        answer2: "Liandry's Torment",
-        answer3: "Luden's Echo",
-        answer4: "Morellonomicon"
+        answers: [
+            "Rylai's Crystal Scepter",
+            "Liandry's Torment",
+            "Luden's Echo",
+            "Morellonomicon"
+        ],
+        correctAnswer: "Luden's Echo",
+        image: "<img src=\"assets/images/Luden.png\" alt=\"Luden's Echo\">"
     },
     {
         question: "Which of the following champions has the highest base health?",
-        answer1: "Zac",
-        answer2: "Alistar",
-        answer3: "Braum",
-        answer4: "Tryndamere"
+        answers: [
+            "Zac",
+            "Alistar",
+            "Braum",
+            "Tryndamere"
+        ],
+        correctAnswer: "Tryndamere",
+        image: "<img src=\"assets/images/Tryndamere.png\" alt=\"Tryndamere\">"
     },
     {
         question: "What is the name of Nunu's yeti?",
-        answer1: "Yoyo",
-        answer2: "Willump",
-        answer3: "Tristy",
-        answer4: "Garen"
+        answers: [
+            "Yoyo",
+            "Willump",
+            "Tristy",
+            "Bristle"
+        ],
+        correctAnswer: "Willump",
+        image: "<img src=\"assets/images/Nunu.jpg\" alt=\"Nunu\">"
     },
     {
         question: "How many champions are currently available in League of Legends?",
-        answer1: "150",
-        answer2: "138",
-        answer3: "141",
-        answer4: "148"
+        answers: [
+            "150",
+            "138",
+            "141",
+            "148"
+        ],
+        correctAnswer: "141",
+        image: "<img src=\"assets/images/Champions.jpg\" alt=\"Champions\">"
     },
     {
         question: "Which of the following abilities can penetrate through Yasuo's Wind Wall?",
-        answer1: "Final Spark",
-        answer2: "Tidal Wave",
-        answer3: "Weaver's Wall",
-        answer4: "Super Mega Death Rocket!"
+        answers: [
+            "Final Spark",
+            "Tidal Wave",
+            "Weaver's Wall",
+            "Super Mega Death Rocket!"
+        ],
+        correctAnswer: "Final Spark",
+        image: "<img src=\"assets/images/Lux.jpg\" alt=\"Final Spark\">"
     },
     {
         question: "How much gold does a completed Infinity Edge cost?",
-        answer1: "3800",
-        answer2: "3700",
-        answer3: "3600",
-        answer4: "3500"
+        answers: [
+            "3800",
+            "3700",
+            "3600",
+            "3500"
+        ],
+        correctAnswer: "3700",
+        image: "<img src=\"assets/images/Infinity.jpg\" alt=\"Infinity Edge\">"
     },
     {
         question: "What is the earliest time that the Elder Dragon can spawn on Summoner's Rift?",
-        answer1: "30 minutes",
-        answer2: "35 minutes",
-        answer3: "37 minutes",
-        answer4: "40 minutes"
+        answers: [
+            "30 minutes",
+            "35 minutes",
+            "37 minutes",
+            "40 minutes"
+        ],
+        correctAnswer: "35 minutes",
+        image: "<img src=\"assets/images/Elder.png\" alt=\"Elder Dragon\">"
     }
 
 ];
@@ -76,42 +116,100 @@ var correctAnswers = 0;
 var wrongAnswers = 0;
 var unanswered = 0;
 var index = 0;
+var timeRemaining = 31;
+var intervalId;
+var answer;
 
-function displayTrivia() {
-    if (index < trivia.length) {
-        $("#questionDisplay").html(trivia[index].question + "<br>");
-        var answerButton1 = $("<input type=\"radio\" name=\"answer\" value=\"answer 1\">" +
-            " " + trivia[index].answer1 + "<br>");
-        var answerButton2 = $("<input type=\"radio\" name=\"answer\" value=\"answer 1\">" +
-            " " + trivia[index].answer2 + "<br>");
-        var answerButton3 = $("<input type=\"radio\" name=\"answer\" value=\"answer 1\">" +
-            " " + trivia[index].answer3 + "<br>");
-        var answerButton4 = $("<input type=\"radio\" name=\"answer\" value=\"answer 1\">" +
-            " " + trivia[index].answer4 + "<br>");
-        $(answerButton1).addClass("answer-button");
-        $(answerButton2).addClass("answer-button");
-        $(answerButton3).addClass("answer-button");
-        $(answerButton4).addClass("answer-button");
-        $("#questionDisplay").append(answerButton1);
-        $("#questionDisplay").append(answerButton2);
-        $("#questionDisplay").append(answerButton3);
-        $("#questionDisplay").append(answerButton4);
+function stopTimer() {
+    clearInterval(intervalId);
+}
 
-        index++;
+function startGame() {
+    $("#start").addClass("hide");
+    $("#startOver").addClass("hide");
+    $("#questionDisplay").removeClass("hide");
+    wrongAnswers = 0;
+    index = 0;
+    correctAnswers = 0;
+    unanswered = 0;
+    timeRemaining = 31;
+    game();
+}
+
+function game() {
+    $("#questionDisplay").html(trivia[index].question + "<br>");
+    for (k = 0; k < 4; k++) {
+        var answerButton = $("<button>");
+        $(answerButton).append(trivia[index].answers[k]);
+        $(answerButton).addClass("answer-button");
+        $(answerButton).attr("data-answer", trivia[index].answers[k]);
+        $("#questionDisplay").append(answerButton);
+        $("#questionDisplay").append("<br>");
+        answer = trivia[index].correctAnswer;
+    }
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    timeRemaining--;
+    $("#timeDisplay").html("<h2>Time Remaining: " + timeRemaining + " seconds</h2>")
+    if (timeRemaining === 0) {
+        clearInterval(intervalId);
+        unanswered++;
+        displayOutOfTime();
+        timeRemaining = 31;
     }
 }
-function idk() {
-    $("#start").addClass("hide");
-    $("#questionDisplay").removeClass("hide");
-    setInterval(displayTrivia, 1000);
+
+function displayCorrect() {
+    $("#questionDisplay").html("Correct!<br>" + trivia[index].image);
+    if (index === 9) {
+        setTimeout(completed, 5000)
+    }
+    index++;
+    setTimeout(game, 5000);
 }
 
-$("#start").on("click", function () {
-    $("#start").addClass("hide");
-    $("#questionDisplay").removeClass("hide");
-    setInterval(displayTrivia, 1000);
+function displayWrong() {
+    $("#questionDisplay").html("Nope!<br>" + "The correct answer was " + trivia[index].correctAnswer + "<br>" + trivia[index].image);
+    if (index === 9) {
+        setTimeout(completed, 5000)
+    }
+    index++;
+    setTimeout(game, 5000);
+}
+
+function displayOutOfTime() {
+    $("#questionDisplay").html("Out of time!<br>" + "The correct answer was " + trivia[index].correctAnswer + "<br>" + trivia[index].image);
+    if (index === 9) {
+        setTimeout(completed, 5000)
+    }
+    index++;
+    setTimeout(game, 5000);
+}
+
+function completed() {
+    $("#startOver").removeClass("hide");
+    $("#questionDisplay").html("<h2>All done, here's how you did!</h2><br>" + "<h4>Correct Answers: " + correctAnswers + "</h4><br><h4>Incorrect Answers: " + wrongAnswers + "</h4><br><h4> Unanswered: " + unanswered + "</h4>");
+}
+
+$("#start").click(startGame);
+
+$(document).on("click", ".answer-button", function () {
+    clearInterval(intervalId);
+    var userAnswer = $(this).attr("data-answer");
+    if (userAnswer === answer) {
+        correctAnswers++;
+        displayCorrect();
+    } else {
+        wrongAnswers++;
+        displayWrong();
+    }
+    timeRemaining = 31;
 });
 
+$("#startOver").click(startGame);
 
 
 
@@ -120,15 +218,110 @@ $("#start").on("click", function () {
 
 
 
-        // $("#questionDisplay").html(
-        //     trivia[index].question +
-        //     "<br>" +
-        //     "<input type=\"radio\" name=\"answer\" value=\"answer 1\">" +
-        //     " " + trivia[index].answer1 + "<br>" +
-        //     "<input type=\"radio\" name=\"answer\" value=\"answer 2\">" +
-        //     " " + trivia[index].answer2 + "<br>" +
-        //     "<input type=\"radio\" name=\"answer\" value=\"answer 3\">" +
-        //     " " + trivia[index].answer3 + "<br>" +
-        //     "<input type=\"radio\" name=\"answer\" value=\"answer 4\">" +
-        //     " " + trivia[index].answer4 + "<br>"
-        // );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function stop() {
+//     clearInterval(intervalId);
+// }
+
+// function game() {
+//     $("#questionDisplay").html(trivia[index].question + "<br>");
+//     for (k = 0; k < 4; k++) {
+//         var answerButton = $("<button>");
+//         $(answerButton).append(trivia[index].answers[k]);
+//         $(answerButton).addClass("answer-button");
+//         $(answerButton).attr("data-answer", trivia[index].answers[k]);
+//         $("#questionDisplay").append(answerButton);
+//         $("#questionDisplay").append("<br>");
+//         answer = trivia[index].correctAnswer;
+//     }
+//     index++;
+// }
+
+// function start() {
+//     $("#start").addClass("hide");
+//     $("#questionDisplay").removeClass("hide");
+//     intervalId = setInterval(decrement, 1000);
+// }
+
+// function decrement() {
+//     timeRemaining--;
+//     $("#timeDisplay").html("Time remaining: " + timeRemaining + "seconds");
+//     if (timeRemaining === 0) {
+//         unanswered++;
+//         timeRemaining = 30;
+//         game();
+//     }
+// }
+
+// $(document).on("click", ".answer-button", function () {
+//     var userAnswer = $(this).attr("data-answer");
+//     if (userAnswer === answer) {
+//         correctAnswers++;
+//     } else {
+//         wrongAnswers++;
+//     }
+//     game();
+//     timeRemaining = 30;
+// });
+
+// $("#start").on("click", function () {
+//     start();
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
